a6b16e1e9c7af1c9cb9df8768ae90a5c41c7817c
kouta-wolf <koutaookami18@gmail.com>
2026-02-24 15:34:59 +0900

    Add: indexページに新規作成フォームを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,21 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+    @article = Article.new
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      @articles = Article.all
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def new
@@ -12,4 +23,10 @@ class ArticlesController < ApplicationController
 
   def edit
   end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
