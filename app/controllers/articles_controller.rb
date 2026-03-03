57cccae8e7ef1c06917066555791008248649c90
kouta-wolf <koutaookami18@gmail.com>
2026-03-03 11:33:42 +0900

    Add: 編集画面の追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,16 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
