3a027b475b6056463c6ff76ed3889c739feb2f30
kouta-wolf <koutaookami18@gmail.com>
2026-03-03 11:37:12 +0900

    Fix: Before_actionで関数をまとめた

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @articles = Article.all
     @article = Article.new
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -19,7 +21,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to root_path, status: :see_other
   end
@@ -28,11 +29,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to article_path(@article)
     else
@@ -43,6 +42,10 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
