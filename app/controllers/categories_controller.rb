ad91fe3ce700e0a40c36870daadaec5dc7cada24
kouta-wolf <koutaookami18@gmail.com>
2026-03-04 14:22:30 +0900

    Change: カテゴリの管理・追加ページを作成

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,21 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      @categories = Category.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
