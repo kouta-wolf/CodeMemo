5f800187994fa2f88014be6185a8f730736ec46b
kouta-wolf <koutaookami18@gmail.com>
2026-03-04 13:52:07 +0900

    Change: seed.rbを修正（カテゴリー）

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
-# 既存のデータを一度消去（何度も実行できるようにするため）
+# 1. 既存データを消去
 Article.destroy_all
+Category.destroy_all
 
-# ダミーデータを複数作成
+# 2. カテゴリを作成し、変数に入れる
+ruby = Category.create!(name: 'Ruby')
+rails = Category.create!(name: 'Rails')
+git = Category.create!(name: 'Git')
+
+# 3. 記事を作成する際、どのカテゴリに属するかを指定
 Article.create!([
-  { title: "Rubyの基礎", content: "変数やメソッドの書き方を学んだ。" },
-  { title: "Railsのルーティング", content: "get 'url', to: 'controller#action' の書き方。" },
-  { title: "GitHubの使い方", content: "add, commit, pushの流れを覚えた。" }
+  { title: "Rubyの基礎", content: "変数やメソッドの書き方を学んだ。", category: ruby },
+  { title: "Railsのルーティング", content: "get 'url', to: 'controller#action' の書き方。", category: rails },
+  { title: "GitHubの使い方", content: "add, commit, pushの流れを覚えた。", category: git }
 ])
 
-puts "ダミーデータの作成が完了しました！"
+puts "カテゴリと記事のダミーデータ作成が完了しました！"
