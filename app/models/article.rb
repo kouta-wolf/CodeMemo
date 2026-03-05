fb42b366809b89759bedff8fc5c98fa96683345b
kouta-wolf <koutaookami18@gmail.com>
2026-03-05 10:56:12 +0900

    Fix: エラー表示をフォーム下に個別表示、カテゴリエラーも赤くなるように設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ApplicationRecord
 
   belongs_to :category
   
-  # タイトル、内容が空なのを許容しない
+  # タイトル、内容、カテゴリが空なのを許容しない
   validates :title, presence: true
   validates :content, presence: true
+  validates :category_id, presence: true
 end
