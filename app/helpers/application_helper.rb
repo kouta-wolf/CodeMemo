1def3c6ab3bf8ea9430813b5eb2ba6baa5f89fa8
kouta-wolf <koutaookami18@gmail.com>
2026-03-05 10:38:20 +0900

    Add: 動的タイトルを一部設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title(title= '')
+    base_title = 'CodeMemo'
+    if title.empty?
+      base_title
+    else
+      "#{title} | #{base_title}"
+    end
+  end
 end
